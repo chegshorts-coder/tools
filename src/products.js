cb20dd4719ef682f280d5ab9bf3be3b99f57e8e3
chegshorts-coder <chegshorts@gmail.com>
2026-06-18 17:46:35 +0100

    Update products with full product list

--- src/products.js
+++ src/products.js
@@ -5,9 +5,39 @@ const products = [
     link: "https://amzn.to/3QaqiTz"
   },
   {
+    name: "Best Foil Shaver",
+    image: "/images/best-foil.jpg",
+    link: "https://amzn.to/4vlztjg"
+  },
+  {
+    name: "Trimmer Pick",
+    image: "/images/trimmer.jpg",
+    link: "https://amzn.to/4a6Y9Ds"
+  },
+  {
     name: "Best Trimmer",
     image: "/images/best-trimmer.jpg",
     link: "https://amzn.to/4exqGTY"
+  },
+  {
+    name: "Budget Clipper",
+    image: "/images/budget-clipper.jpg",
+    link: "https://amzn.to/4vhLPbU"
+  },
+  {
+    name: "Best Clipper",
+    image: "/images/best-clipper.jpg",
+    link: "https://amzn.to/4oDNjuK"
+  },
+  {
+    name: "Budget Lighting",
+    image: "/images/budget-lighting.jpg",
+    link: "https://amzn.to/3SkafDa"
+  },
+  {
+    name: "Best Lighting",
+    image: "/images/best-lighting.jpg",
+    link: "https://amzn.to/4fYuujL"
   }
 ];
 
