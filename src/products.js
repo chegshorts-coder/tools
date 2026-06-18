cbe1999ba95783f9d1e2b93d4e7ca3044698b46c
chegshorts-coder <chegshorts@gmail.com>
2026-06-18 17:56:13 +0100

    Update product image paths to match renamed images

--- src/products.js
+++ src/products.js
@@ -11,32 +11,32 @@ const products = [
   },
   {
     name: "Trimmer Pick",
-    image: "/images/trimmer.jpg",
+    image: "/images/trimmer.png",
     link: "https://amzn.to/4a6Y9Ds"
   },
   {
     name: "Best Trimmer",
-    image: "/images/best-trimmer.jpg",
+    image: "/images/best-trimmer.png",
     link: "https://amzn.to/4exqGTY"
   },
   {
     name: "Budget Clipper",
-    image: "/images/budget-clipper.jpg",
+    image: "/images/budget-clipper.png",
     link: "https://amzn.to/4vhLPbU"
   },
   {
     name: "Best Clipper",
-    image: "/images/best-clipper.jpg",
+    image: "/images/best-clipper.png",
     link: "https://amzn.to/4oDNjuK"
   },
   {
     name: "Budget Lighting",
-    image: "/images/budget-lighting.jpg",
+    image: "/images/budget-lighting.png",
     link: "https://amzn.to/3SkafDa"
   },
   {
     name: "Best Lighting",
-    image: "/images/best-lighting.jpg",
+    image: "/images/best-lighting.png",
     link: "https://amzn.to/4fYuujL"
   }
 ];
