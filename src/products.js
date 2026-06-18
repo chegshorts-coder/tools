ef3192cdd92620ed827263c5151921e4198098a4
chegshorts-coder <chegshorts@gmail.com>
2026-06-18 17:30:15 +0100

    Add Best Trimmer product

--- src/products.js
+++ src/products.js
@@ -3,6 +3,11 @@ const products = [
     name: "Budget Foil Shaver",
     image: "/images/budget-foil.jpg",
     link: "https://amzn.to/3QaqiTz"
+  },
+  {
+    name: "Best Trimmer",
+    image: "/images/best-trimmer.jpg",
+    link: "https://amzn.to/4exqGTY"
   }
 ];
 
